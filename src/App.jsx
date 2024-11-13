7150d3621ba45bc801e5ff188a98bdb2064b43ce
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-11-13 21:36:35 +0300

    React router dom inclusion and files update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 
-import "./layout.scss"
-import Navbar from "./components/navbar/Navbar.jsx"
-import  HomePage from "./routes/homePage/HomePage.jsx"
+// import "./layout.scss";
+import Navbar from "./components/navbar/Navbar.jsx";
+import  HomePage from "./routes/homePage/HomePage.jsx";
+import listPage from "./routes/listPage/listPage.jsx";
+ import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link
+ } from "react-router-dom"
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage/>,
+    },
+    {
+      path: "/list",
+      element: <listPage/>,
+    },
+  ]);
+
   return (
-    <div className="layout">  
-      <div className="navbar">
-      <Navbar/>
-      </div>  
-      <div className="content"><HomePage/>
-      </div>  
-    </div>
+    // <div className="layout">  
+    //   <div className="navbar">
+    //   <Navbar/>
+    //   </div>  
+    //   <div className="content"><HomePage/>
+    //   </div>  
+    // </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/routes/layout/layout.jsx
@@ -0,0 +1,13 @@
+ import Navbar from "../../components/navbar/Navbar"
+ import "../../layout.scss"
+function Layout() {
+  return (
+     <div className="layout">  
+      <div className="navbar">
+      <Navbar/>
+      </div>      
+    </div>
+  )
+}
+
+export default Layout
