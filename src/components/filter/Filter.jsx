81ec51789d085ff54c9b18f0427d1ab60ab5d35e
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-11-14 23:52:04 +0300

    Created filter section

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,9 @@
+import "./filter.scss";
+
+function Filter() {
+  return (
+    <div>Filter</div>
+  )
+}
+
+export default Filter
--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,12 +1,16 @@
 import './listPage.scss';
 import { listData } from '../../lib/dummydata';
-
+import Filter from '../../components/filter/Filter';
 function ListPage(){
     const data = listData
     return (
         <div className="listPage">
             <div className = "listContainer">
-                <div className='wrapper'></div>
+                <div className='wrapper'>
+                    <div>
+                      <Filter/>  
+                    </div>
+                </div>
             </div>
             <div className = "mapContainer">Map</div>
         </div>
