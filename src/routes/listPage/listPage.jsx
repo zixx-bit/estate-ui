dc5c8d19f9817087fc2fe6d0eb51eaa74fa9b680
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-11-14 23:41:57 +0300

    list page

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -5,7 +5,9 @@ function ListPage(){
     const data = listData
     return (
         <div className="listPage">
-            <div className = "listContainer">List</div>
+            <div className = "listContainer">List
+                <div className='wrapper'></div>
+            </div>
             <div className = "mapContainer">Map</div>
         </div>
     )
