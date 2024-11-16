9fd09e3f6cedd7da242ee0eb65909fb0f63b7f38
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-11-17 00:20:10 +0300

    filter page update

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -2,9 +2,10 @@ import "./Filter.scss";
 
 function Filter() {
   return (
-    <div className="filter">
+  <div className="filter">
     <div>
-      <h1>Search rsults for<b/>London</h1>
+      <h1>Search results for <b>London</b>
+      </h1>
     </div>
     {/* top */}
     <div className="top">
@@ -14,7 +15,7 @@ function Filter() {
     </div>
     </div>
     {/* bottom */}
-    <div className="bottom">
+  <div className="bottom">
     <div className="item">
       <label htmlFor="type">Type</label>
       <select name="type" id="type">
@@ -23,9 +24,49 @@ function Filter() {
         <option value="">Rent</option>
       </select>
     </div>
-
+ 
+    {/* property */}
+    <div className="item">
+      <label htmlFor="property">Property</label>
+      <select name="propery" id="property">
+      <option value="">any</option>
+      <option value="">Apartment</option>
+      <option value="">Condo</option>
+      <option value="">House</option>
+      </select>
     </div>
+   {/* min price */}
+    <div className="item">
+      <label htmlFor="minPrice">Min price</label>
+      <select name="minPrice" id="minPrice">
+      <option value="">any</option>
+      <option value="">Apartment</option>
+      <option value="">Condo</option>
+      <option value="">House</option>
+      </select>
     </div>
+{/* max price */}
+    <div className="item">
+      <label htmlFor="maxPrice">Max price</label>
+      <select name="maxPrice" id="maxPrice">
+      <option value="">any</option>
+      <option value="">Apartment</option>
+      <option value="">Condo</option>
+      <option value="">House</option>
+      </select>
+    </div>
+    {/* bedroom */}
+    <div className="item">
+      <label htmlFor="bedroom">Bedroom</label>
+      <select name="bedroom" id="bedroom">
+      <option value="">any</option>
+      <option value="">Apartment</option>
+      <option value="">Condo</option>
+      <option value="">House</option>
+      </select>
+    </div>
+  </div>
+</div>
   )
 }
 
