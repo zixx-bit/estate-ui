7b4ef5343e5e85f61d06a79fbd1c14f34d57d125
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-11-16 23:12:50 +0300

    Filter page update

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -2,7 +2,30 @@ import "./Filter.scss";
 
 function Filter() {
   return (
-    <div>Filter</div>
+    <div className="filter">
+    <div>
+      <h1>Search rsults for<b/>London</h1>
+    </div>
+    {/* top */}
+    <div className="top">
+    <div className="item">
+      <label htmlFor="City">Location</label>
+      <input type="text" id="city" name="city" placeholder="City location"></input>
+    </div>
+    </div>
+    {/* bottom */}
+    <div className="bottom">
+    <div className="item">
+      <label htmlFor="type">Type</label>
+      <select name="type" id="type">
+        <option value="">any</option>
+        <option value="">Buy</option>
+        <option value="">Rent</option>
+      </select>
+    </div>
+
+    </div>
+    </div>
   )
 }
 
