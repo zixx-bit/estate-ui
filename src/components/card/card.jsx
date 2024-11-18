1f12531a8ef0f4afb4366c5c56f17df108261141
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-11-18 22:06:53 +0300

    list page update

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -7,8 +7,43 @@ function Card({property}){
     <Link to={ `/${property.id}` } className="imageContainer">
       <img src={property.images} alt=""/>
       </Link>
-      <div className="textContainer"></div>
-    </div>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${property.id}`}>{property.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="/pin.png" alt=""></img>
+           <span>{property.address}</span> 
+        </p>
+        <p className="price">${property.price}</p>
+          <div className="bottom">
+            <div className="features">
+              <div className="feature">
+              <img src="/bed.png" alt=""></img>
+              <span>{property.bedrooms} bedroom</span>
+              </div>
+             
+              <div className="feature">
+              <img src="/bath.png" alt=""></img>
+              <span>{property.bathrooms} bathroom</span>
+              </div>
+             </div>
+
+              <div className="icons">
+              <div className="icon">
+                <img src="/save.png" alt=""></img>
+              </div> 
+              <div className="icon">
+                <img src="/chat.png" alt=""></img>
+              </div>           
+              </div>                        
+
+            </div>
+
+          </div>
+
+      </div>
+  
   )
 }
 
