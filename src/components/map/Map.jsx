39c642ef60b0e85fddea473ca2c51637593d2536
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-11-21 13:26:16 +0300

    map pin file updated dynamic marker and popup details

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,19 +1,19 @@
 import "./map.scss";
 import "leaflet/dist/leaflet.css"
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import Pin from "../pin/Pin.jsx";
 
-const Map = () => {
+const Map = ({locations}) => {
   return (
     <MapContainer center={[51.5074, -0.1278]} zoom={7} scrollWheelZoom={false} className='map'>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[51.5074, -0.1278]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+    {locations.map( location=>{
+      <Pin location={location} key={location.id}/>
+    })}
+    
   </MapContainer>
   )
 }
---
+++ src/components/pin/Pin.jsx
@@ -0,0 +1,22 @@
+import { Marker, Popup } from "react-leaflet"
+import "./pin.scss"
+import { Link } from "react-router-dom"
+
+const Pin = (location) => {
+  return (        
+    <Marker position={[location.latitude, location.longitude]}>
+      <Popup>
+        <div className="popupContainer">
+          <img ></img>
+          <div className="textContainer">
+            <Link to={`/${location.id}`}>{location.title}</Link>
+            <span className="bed">{location.bedroom}</span>
+            <b>Ksh{location.price}</b>
+          </div>
+        </div>
+      </Popup>
+    </Marker>
+  )
+}
+
+export default Pin
