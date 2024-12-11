8595dbdcaad4ff9ee122031f1e66465b83caff1a
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-12-11 13:34:36 +0300

    single page update. details included

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,11 +1,26 @@
+import Slider from "../../components/Slider/Slider";
 import "./singlePage.scss";
+import { singlePostData } from "../../lib/dummydata";
 
 function singlePage() {
   return (
-    <div className="singlePage">single
+    <div className="singlePage">
       <div className="details">
+      <div className="wrapper"><Slider images={singlePostData.images}/>
+      <div className="info">
+        <div className="top">
+          <div className="post">
+            <h1>{singlePostData.title}</h1>
+          </div>
+          <div className="user"></div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+      </div>
       </div>
       <div className="features">
+      <div className="wrapper"></div>
+
       </div>
     </div>
   )
