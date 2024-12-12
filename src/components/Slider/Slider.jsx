5920c397cd60c63161f284780d90ec03cc3f6f09
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-12-13 00:21:14 +0300

    slider update

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import "./slider.scss";
 
 const Slider = ({images}) => {
   return (
-    <div className='slider'>
-    <div className='bigImage'></div>
-      
+   <div className='slider'>
+    <div className='fullSlider'>
+      <div className='arrow'><img src='/arrow.png'></img></div>
+      <div className='imgContainer'><img src={images[0]}></img></div>
+      <div className='arrow'><img src='/arrow.png' className='rightArrow'></img></div>
+      <div className='close'>X</div>
     </div>
+    <div className='bigImage'>
+      <img src={images[0]}/>
+    </div>
+    <div className='smallImages'>
+      {images.slice(1).map((image, index)=>(<img src={image} alt='' key={index}/>)) }
+    </div>  
+  </div>
   )
 }
 
