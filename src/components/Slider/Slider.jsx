d5e96323a87f341b28abf2a56aaeb675e62c96d4
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2024-12-21 20:47:17 +0300

    slider update

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./slider.scss";
 
 const Slider = ({images}) => {
+  const[imageIndex, setImageIndex] = useState (null)
   return (
-   <div className='slider'>
+     <div className='slider'>
+    {imageIndex !==null && ( 
     <div className='fullSlider'>
       <div className='arrow'><img src='/arrow.png'></img></div>
       <div className='imgContainer'><img src={images[0]}></img></div>
-      <div className='arrow'><img src='/arrow.png' className='rightArrow'></img></div>
-      <div className='close'>X</div>
+      <div className='arrow'> <img src='/arrow.png' className='rightArrow'></img></div>
+      <div className='close' onClick={() => setImageIndex(null) }>X</div>
     </div>
+  )}
     <div className='bigImage'>
-      <img src={images[0]}/>
+      <img src={images[0]} alt='' onClick={() => setImageIndex(2) }/>
     </div>
     <div className='smallImages'>
       {images.slice(1).map((image, index)=>(<img src={image} alt='' key={index}/>)) }
